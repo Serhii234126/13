bd9be66aa54b37616ab03f9ba96dd0ffc7a1273b
Serhii234126 <kievstar719917@gmail.com>
2025-12-12 01:18:17 +0200

    Add files via upload

---
+++ src/components.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled, { ThemeProvider as StyledThemeProvider } from 'styled-components';
+
+// Task 1.1: Create StyledButton
+
+
+// Task 1.2: Create StyledCard
+
+
+// Task 1.3: Create StyledTitle
+
+
+// Task 2.1: Create StyledBox with primary prop
+
+
+// Task 2.2: Create StyledText with size prop
+
+
+// Task 2.3: Create StyledBadge with status prop
+
+
+// Task 3.1: Create BaseButton and PrimaryButton
+
+
+// Task 3.2: Create BaseCard and HighlightCard
+
+
+// Task 4.1: Create ThemeProvider wrapper and ThemedButton
+
+
+// Task 4.2: Create ThemedContainer
+
+
+// Task 5.1: Create HoverButton
+
+
+// Task 5.2: Create FlexContainer
+
+
+// Task 5.3: Create ResponsiveText
+
+
+// Export all styled components
+export {
+    StyledButton,
+    StyledCard,
+    StyledTitle,
+    StyledBox,
+    StyledText,
+    StyledBadge,
+    BaseButton,
+    PrimaryButton,
+    BaseCard,
+    HighlightCard,
+    ThemeProvider,
+    ThemedButton,
+    ThemedContainer,
+    HoverButton,
+    FlexContainer,
+    ResponsiveText
+};
